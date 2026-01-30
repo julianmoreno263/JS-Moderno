37b3f70d7801ab2a6109ea84691a663820d12f78
julian <julianmoreno263@gmail.com>
2026-01-30 12:10:35 -0500

    Creando strings

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,14 @@
+//los strings son cadenas de texto y deben ir entre comillas dobles o sencillas
+const producto="Monitor"
+
+//se pueden crear utilizando el metodo String()
+const producto2=String("Monitor 20 pulgadas")
+
+//tambien utilizando la clase String
+const producto3=new String("Teclado")
+
+
+console.log(producto)
+console.log(producto2)
+console.log(producto3)
+
