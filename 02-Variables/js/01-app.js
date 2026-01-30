e2d1f87206e44bc2de78c5e9060b8e4b3e63dabd
julian <julianmoreno263@gmail.com>
2026-01-30 11:48:25 -0500

    Inicializando repositorio del curso JS Moderno

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,16 @@
+//forma de crear variables en la nueva actualizacion de js es6 con let
+let producto="monitor"
+
+//las variables se pueden reasignar
+producto="teclado"
+
+//js es un lenguaje dinamico, no se necesita esecificar el tipo de dato
+producto=20
+
+console.log(producto)
+
+//se puede inicializar una variable y asignarle valor despues
+let disponible
+disponible=false
+console.log(disponible)
+
