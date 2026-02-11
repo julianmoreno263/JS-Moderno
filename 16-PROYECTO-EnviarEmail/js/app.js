08973cad5874e850c543bea917839113c08188b2
julian <julianmoreno263@gmail.com>
2026-02-11 13:21:49 -0500

    creando spinner, reseteando formulario,simulando envio de email

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     const formulario=document.querySelector("#formulario")
     const btnSubmit=document.querySelector("#formulario button[type='submit']")
     const btnReset=document.querySelector("#formulario button[type='reset']")
+    const spinner=document.querySelector("#spinner")
 
 
     //asignar eventos, usamos el evento blur que se ejecuta cuando el elemento pierde el foco, y el evento input se dispara cuando se esta escribiendo en el campo,es mas para validacion en tiempo real
@@ -27,19 +28,46 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     inputMensaje.addEventListener("input",validar)
 
+    formulario.addEventListener("submit",enviarEmail)
+
     btnReset.addEventListener("click",e=>{
         e.preventDefault()
 
         //reiniciar el objeto
-        email.email=""
-        email.asunto=""
-        email.mensaje=""
-
-        formulario.reset()
-        comprobarEmail()
+        resetFormulario()
 
 
     })
+
+    function enviarEmail(e) {
+        e.preventDefault()
+
+        spinner.classList.add("flex")
+        spinner.classList.remove("hidden")
+
+        console.log("Enviando...")
+
+        setTimeout(() => {
+            spinner.classList.remove("flex")
+            spinner.classList.add("hidden")
+
+            //reiniciar el objeto
+            resetFormulario()
+
+            //crear alerta
+            const alertaExito=document.createElement("p")
+            alertaExito.classList.add("bg-green-500","text-white","p-2","text-center","rounded-lg","mt-10","font-bold","text-sm","uppercase")
+            alertaExito.textContent="Mensaje enviado correctamente"
+
+            formulario.appendChild(alertaExito)
+
+            setTimeout(() => {
+                alertaExito.remove()
+            }, 3000);
+            
+        }, 3000);
+        
+    }
 
 
 
@@ -110,6 +138,17 @@ document.addEventListener("DOMContentLoaded",()=>{
         btnSubmit.classList.remove("opacity-50")
         btnSubmit.disabled=false
         
+    }
+
+    function resetFormulario(params) {
+        //reiniciar el objeto
+        email.email=""
+        email.asunto=""
+        email.mensaje=""
+
+        formulario.reset()
+        comprobarEmail()
+
     }
 
 })
