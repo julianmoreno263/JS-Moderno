9d14b758f1891d56a98ddee8e45cefd415313f96
julian <julianmoreno263@gmail.com>
2026-04-16 18:40:33 -0500

    Trabajando con modulos, export e import

---
+++ 28-Modulos/js/app.js
@@ -0,0 +1,6 @@
+import { nombreCliente,ahorro, mostrarInformacion } from "./cliente.js";
+
+console.log(nombreCliente)
+console.log(ahorro)
+
+console.log(mostrarInformacion(nombreCliente,ahorro))
---
+++ 28-Modulos/js/cliente.js
@@ -0,0 +1,13 @@
+
+
+  export  const nombreCliente="juan"
+  export const ahorro=200
+
+
+  //funcion para exportar
+  export function mostrarInformacion(nombre,ahorro) {
+    return `Cliente: ${nombre} - Ahorro: ${ahorro}`
+  }
+
+
+
